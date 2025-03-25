59ac3555e778d3720c2b7157c810126a45205e18
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2025-03-25 20:59:29 +0530

    loaded products in beforeAll hook

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage,cart} from '../../data/cart.js';
+import { loadProducts } from '../../data/products.js';
 
 // integrated tests - tests many units/pieces of code working together
 // hooks - share code between 2 tests
@@ -9,6 +10,15 @@ describe('test suite : renderOrderSummary',()=>{
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
     //types of hooks - beforeEach(),afterEach(),beforeAll(),afterAll()
+
+    beforeAll((done)=>{
+        // due to loadproducts is asynchronous, we can use jasmine's done parameter to wait for response
+        // done lets us control when to go to next step
+        loadProducts(()=>{
+            done();
+        });
+    });
+
     beforeEach(()=>{
         spyOn(localStorage,'setItem');
 
